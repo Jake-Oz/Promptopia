bf1d82f55dac3e9523934341b08bec20fb009103
Jake-Oz <jakeandrach@me.com>
2023-05-27 16:15:13 +1000

    fixed typo

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -3,7 +3,7 @@
 import { useState, UseEffect, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
-import Profile from "@components/profile";
+import Profile from "@components/Profile";
 
 const OtherProfile = ({ params }) => {
   const [posts, setPosts] = useState([]);
