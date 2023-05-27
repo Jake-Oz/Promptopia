3e8029252b574233b66937e393ef5281daed6a90
Jake-Oz <jakeandrach@me.com>
2023-05-27 16:08:14 +1000

    First Committ

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState, UseEffect, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/profile";
+
+const OtherProfile = ({ params }) => {
+  const [posts, setPosts] = useState([]);
+
+  const router = useRouter();
+  const queryName = new URL(document.location).searchParams.get("name");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <Profile
+      name={queryName}
+      desc={`Welcome to ${queryName}'s personalised profile page. 
+      Explore ${queryName}'s exceptional prompts and be inspired
+      by the power of their imagination.`}
+      data={posts}
+    />
+  );
+};
+export default OtherProfile;
---
+++ components/Feed.jsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+import PromptCard from "./PromptCard";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+
+const PromptCardList = ({ data, handleTagClick, handleProfileClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+          handleProfileClick={handleProfileClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  const router = useRouter();
+  const { data: session } = useSession();
+  const handleSearchChange = (e) => {
+    e.preventDefault();
+    setSearchText(e.target.value);
+  };
+
+  useEffect(() => {
+    handleTextChange();
+  }, [searchText]);
+
+  const handleTextChange = () => {
+    const filterPosts = posts.filter((post) => {
+      return (
+        post.prompt.toLowerCase().includes(searchText.toLowerCase()) ||
+        post.creator.username.includes(searchText) ||
+        post.tag.toLowerCase().includes(searchText.toLowerCase())
+      );
+    });
+    setFilteredPosts(filterPosts);
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    fetchPosts();
+  }, []);
+
+  const handleProfileClick = (post) => {
+    if (session?.user.id === post.creator._id) return router.push("/profile");
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a prompt, tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList
+        data={searchText ? filteredPosts : posts}
+        handleTagClick={setSearchText}
+        handleProfileClick={handleProfileClick}
+      />
+    </section>
+  );
+};
+export default Feed;
